32eebccca1b607e3dcf528765187432c9a8cc5eb
ArranHL <47016295+ArranHL@users.noreply.github.com>
2019-09-30 15:53:14 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,8 +27,8 @@ resource "aws_instance" "default" {
 }
 
 # Create Security Group for EC2
-resource "aws_security_group" "TerraformJenkins20190930" {
-  name = "terraform-default-sg"
+resource "aws_security_group" "default" {
+  name = "terraform-default-sg" 
 
   
   ingress {
