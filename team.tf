d9b560335dbb49cf95d197a553caf637589822ae
ArranHL <47016295+ArranHL@users.noreply.github.com>
2019-10-04 15:08:38 +0300

    Update team.tf

--- team.tf
+++ team.tf
@@ -1,4 +1,52 @@
- # Adding a Memeber to an Org and Team
+# Terraform state will be stored in S3
+terraform {
+  backend "s3" {
+    bucket = "terraform-bucket-arran"
+    key    = "AKIAJ4AZ2ADQ35GBPS2Q"
+    region = "eu-west-3"
+  }
+}
+
+# Use AWS Terraform provider
+provider "aws" {
+  region = "eu-west-3"
+}
+
+# Create EC2 instance
+resource "aws_instance" "default" {
+  ami                    = "${var.ami}"
+  count                  = "${var.notcount}"
+  key_name               = "${var.key_name}"
+  vpc_security_group_ids = ["${aws_security_group.default.id}"]
+  source_dest_check      = false
+  instance_type          = "${var.instance_type}"
+
+  tags = {
+    Name = "terraform-default"
+  }
+}
+
+# Create Security Group for EC2
+resource "aws_security_group" "default" {
+  name = "terraform-default-sg" 
+
+  
+  ingress {
+    from_port   = 80
+    to_port     = 82
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+    ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
 provider "github" {
 organization = "CrackingKraken"
   
