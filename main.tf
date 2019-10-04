0b54cae26d9943093d6c2d94d2160b79465ed685
ArranHL <47016295+ArranHL@users.noreply.github.com>
2019-10-04 14:43:46 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -47,20 +47,4 @@ resource "aws_security_group" "default" {
 
 }
 
- # Adding a Memeber to an Org and Team
-provider "github" {
-  
-resource "github_membership" "membership_for_some_user" {
-  username = "ArranTest"
-  role     = "member"
-}
-resource "github_team" "some_team" {
-  name        = "bteam"
-  description = "Not A team… But Not C team either"
-  }
-resource "github_team_membership" "bteam_membership" {
-  team_id  = "${3419528}"
-  username = "ArranTest"
-  role     = "member"
-}
-  }
+
