4751b6ada6706984f8d2184c9a9517379b204f92
ArranHL <47016295+ArranHL@users.noreply.github.com>
2019-10-07 12:07:38 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@ variable "notcount" {
   default = 1
 }
 
-variable "JenkinsKey20191007" {
+variable "key_name" {
   description = "Private key name to use with instance"
   default     = "JenkinsKey20191007"
 }
