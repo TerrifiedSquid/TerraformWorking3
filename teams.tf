54b61807a95d87738a749bde75dda520c071ce5e
ArranHL <47016295+ArranHL@users.noreply.github.com>
2019-10-07 14:08:52 +0300

    Update teams.tf

--- teams.tf
+++ teams.tf
@@ -6,17 +6,9 @@ organization = "TerrifiedSquid"
 
 # Adding member to team
 
-resource "github_membership" "membership_ArranTest" {
-  username = "ArranTest"
-  role     = "member"
-}
-resource "github_team" "bteam" {
-  name        = "SomeTeam"
-  description = "Some cool team"
-}
-resource "github_team_membership" "bteam_membership" {
-  team_id  = "${3419528}"
-  username = "ArranTest"
-  role     = "member"
+resource "github_team" "some_team" {
+  name        = "squid1"
+  description = "way down on the ocean floor"
+  privacy     = "closed"
 }
   }  
