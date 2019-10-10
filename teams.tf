2c6eeb57e36ae8b6b60381843c7db98f57a32fca
ArranHL <47016295+ArranHL@users.noreply.github.com>
2019-10-10 15:24:30 +0300

    Update teams.tf

--- teams.tf
+++ teams.tf
@@ -1,6 +1,6 @@
  provider "github" {
  organization = "${var.github_organization}"
- token        = "${var.github_token}"  
+ token        = "${var.TOKEN}"  
   } 
 
 resource "github_membership" "ArranTest" {
