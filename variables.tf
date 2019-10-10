1e1aa211bb39377e38a1c745a31d29f7acc48b9c
ArranHL <47016295+ArranHL@users.noreply.github.com>
2019-10-10 15:25:09 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "key_name" {
 # The following variable is meant to point back to  
 variable "github_token" {
   description = "Token for Access"
-   default     = "GitHubToken"
+   default     = "TOKEN"
 }
 
 variable "instance_type" {
