342c0a920a5ff81aa8f5fa02232311986dc32178
ArranHL <47016295+ArranHL@users.noreply.github.com>
2019-10-18 16:23:42 +0300

    Update teams.tf

--- teams.tf
+++ teams.tf
@@ -3,11 +3,15 @@
  token        = "${var.TOKEN}"  
   } 
 
-resource "github_membership" "ArranTest" {
-username = "ArranTest"
-role = "member" 
+// resource "github_membership" "ArranTest" {
+// username = "ArranTest"
+// role = "member" 
+// }
+
+
+resource "github_team" "some_team" {
+  name        = "some-team"
+  description = "Some cool team"
+  privacy     = "closed"
 }
-
-
-
 
